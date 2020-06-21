df18ae511a6656d920ac43117f520ca90c0dfa1b
Celina Jackson <celinanjackson@gmail.com>
2020-06-21 22:56:16 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,5 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+def parrot (Pretty Bird, greeting = "Squawk!")
+  puts greeting
+  return "Squawk!"
+  return parrot
+end
